8bc8f1f4d8283ffd38b5c3c07a9c299d4a48370e
Mark Crovella <crovella@cs.bu.edu>
2015-04-02 15:15:10 -0400

    L15 final

--- slideUtilities.py
+++ slideUtilities.py
@@ -1,5 +1,7 @@
 from IPython.display import display_html
 from IPython.display import display
+import matplotlib.pyplot as plt
+from IPython.display import HTML
 
 def hide_code_in_slideshow():
     import os
@@ -20,3 +22,35 @@ def hide_code_in_slideshow():
         });
     </script>""" % (uid, uid)
     display_html(html, raw=True)
+
+##########################
+# python notebook does not support matplotlib animations
+# so these functions create an .mp4 video and then display
+# it using inline HTML
+##########################
+
+# Source: http://nbviewer.ipython.org/url/jakevdp.github.io/downloads/notebooks/AnimationEmbedding.ipynb
+from tempfile import NamedTemporaryFile
+
+VIDEO_TAG = """<video controls>
+ <source src="data:video/x-m4v;base64,{0}" type="video/mp4">
+ Your browser does not support the video tag.
+</video>"""
+
+def anim_to_html(anim):
+    if not hasattr(anim, '_encoded_video'):
+        with NamedTemporaryFile(suffix='.mp4') as f:
+            anim.save(f.name, fps=20, extra_args=['-vcodec', 'libx264'])
+            video = open(f.name, "rb").read()
+        anim._encoded_video = video.encode("base64")
+    
+    return VIDEO_TAG.format(anim._encoded_video)
+
+def display_animation(anim):
+    plt.close(anim._fig)
+    return HTML(anim_to_html(anim))
+
+def display_saved_anim(fname):
+    with open(fname,'rb') as f:
+        video = f.read()
+    return HTML(VIDEO_TAG.format(video.encode("base64")))
