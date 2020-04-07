5553fc743e9471b3d44889fbf21544ef22621664
Mark Crovella, home machine <crovella@cs.bu.edu>
2020-04-07 15:10:05 -0400

    L19 preclass

--- laUtilities.py
+++ laUtilities.py
@@ -106,6 +106,35 @@ class two_d_figure:
         self.ax.plot(bounds[0][0],bounds[1][0],'')
         self.ax.plot(bounds[0][1],bounds[1][1],'')
 
+def set_axes_equal(ax):
+    '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc..  This is one possible solution to Matplotlib's
+    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+
+    Input
+      ax: a matplotlib axis, e.g., as output from plt.gca().
+
+      from https://stackoverflow.com/questions/13685386/matplotlib-equal-unit-length-with-equal-aspect-ratio-z-axis-is-not-equal-to
+    '''
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = abs(x_limits[1] - x_limits[0])
+    x_middle = np.mean(x_limits)
+    y_range = abs(y_limits[1] - y_limits[0])
+    y_middle = np.mean(y_limits)
+    z_range = abs(z_limits[1] - z_limits[0])
+    z_middle = np.mean(z_limits)
+
+    # The plot bounding box is a sphere in the sense of the infinity
+    # norm, hence I call half the max range the plot radius.
+    plot_radius = 0.5*max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+    
 class three_d_figure:
     
     def __init__ (self,
@@ -130,6 +159,10 @@ class three_d_figure:
         if self.qr == None:
             # only plot the figure, no QR code
             self.ax = self.fig.add_subplot(111, projection='3d')
+                                               #, proj_type='ortho')
+            # this is not implemented in mp3d as of Apr 2020
+            # self.ax.set_aspect('equal')
+            set_axes_equal(self.ax)
         else:
             # plot the figure and the QR code next to it
             self.ax = self.fig.add_subplot(121, projection='3d', position=[0,0,1,1])
@@ -611,6 +644,7 @@ class three_d_figure:
                                     repeat=False)
         
     def save(self, file_name, qrviz = None):
+        set_axes_equal(self.ax)
         fname = 'json/{}.json'.format(file_name)
         with open(fname, 'w') as fp:
             json.dump(self.desc, fp, indent=2)
