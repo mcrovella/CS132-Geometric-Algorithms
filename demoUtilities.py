5b35a77fa52dfec2ae6d4457e17b67b167cc41e5
Mark Crovella <crovella@cs.bu.edu>
2021-02-09 10:41:37 -0500

    L07 to book

--- demoUtilities.py
+++ demoUtilities.py
@@ -65,8 +65,6 @@ def centerAxes (ax):
     ax.spines['right'].set_color('none')
     ax.spines['bottom'].set_position('zero')
     ax.spines['top'].set_color('none')
-    ax.spines['left'].set_smart_bounds(True)
-    ax.spines['bottom'].set_smart_bounds(True)
     ax.xaxis.set_ticks_position('bottom')
     ax.yaxis.set_ticks_position('left')
     bounds = np.array([ax.axes.get_xlim(), ax.axes.get_ylim()])
