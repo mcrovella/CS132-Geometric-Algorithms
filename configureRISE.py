dfb8a6ee0d27fada969726e428826dea08375794
Mark Crovella, home machine <crovella@cs.bu.edu>
2021-03-17 18:46:07 -0400

    L14 moderate revision

--- configureRISE.py
+++
@@ -1,8 +0,0 @@
-from IPython.html.services.config import ConfigManager
-from IPython.utils.path import locate_profile
-cm = ConfigManager(profile_dir=locate_profile(get_ipython().profile))
-cm.update('livereveal', {
-              'theme': 'beige',
-              'transition': 'fade',
-              'start_slideshow_at': 'selected',
-})
